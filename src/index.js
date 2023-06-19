164b7cb98479ce99c8d951bf874037a6a0a44c2a
Roizor <aidenjs9@icloud.com>
2023-06-19 11:59:09 -0500

    add if

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ let outStr = "//Redditlang Interpreter v0\n";
 fs.readFileSync("main.redditlang").toString().split('\n').forEach(line => {
     // Parse the line
     const output = require('./mod/System').method(line);
+    if(output == '') return;
     outStr += output+"\n";
 });
 console.log(outStr);
--- src/mod/System.js
+++ src/mod/System.js
@@ -3,6 +3,24 @@ module.exports = {
     method: (line) => {
         const keyword = line.split(' ')[0];
         const args = line.split(' ');
+        // Remove comments just cause
+        if (line.startsWith('#')) {
+            return ``
+        } else if (line.includes('#')) {
+            line = line.split('#')[0];
+        }
+
+        if (line.includes('okbud')) {
+            line = line.replace(/okbud/g, 'true')
+        }
+        if (line.includes('okn\'tbud')) {
+            line = line.replace(/okn'tbud/g, 'false')
+        }
+        if (line.includes('wut')) {
+            line = line.replace(/wut/g, 'null')
+        }
+
+
         if (line.startsWith('extuse')) {
             // Loading an extension, is it from us?
             if (args[0].startsWith('js_')) {
@@ -14,6 +32,13 @@ module.exports = {
         } else if (line.startsWith('coitusinterruptus')) {
             const startStr = line.split("'")[1];
             return "console.log('" + startStr + "')"
+        } else if (line.startsWith('is')) {
+            // yummy conditaitonal
+            const checking = line.split('is ')[1].trim();
+            const conds = checking.split(' ');
+            return `if (${conds[0]} == ${conds[1]}) {`
+        } else if(line.startsWith("si")) {
+            return `}`
         }
     }
 }
