4dd0181139638542708a1cb4b132aad997b60249
Roizor <aidenjs9@icloud.com>
2023-06-19 13:26:59 -0500

    sdfg

--- src/Redditmod.js
+++ src/Redditmod.js
@@ -1,4 +1,5 @@
 const invalid = {
-    'mohsenemx': 'Fuck linux, windows is better',
-    'moosedude': 'freedom of choice is just an illusion, the earth is flat, cheese is a right wing fantasy creation to control the left',
+    'roizor': [ 'Twitter is better with Elon' ],
+    'mohsenemx': [ 'Fuck linux, windows is better' ],
+    'moosedude': [ 'freedom of choice is just an illusion', 'the earth is flat', 'cheese is a right wing fantasy creation to control the left' ]
 }
