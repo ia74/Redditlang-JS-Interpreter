3082f79e9df67335f52a31bcf63f57d69e896240
Roizor <aidenjs9@icloud.com>
2023-06-19 20:11:10 -0500

    add weight

--- src/Redditmod.js
+++ src/Redditmod.js
@@ -1,5 +1,33 @@
 const invalid = {
-    'roizor': [ 'Twitter is better with Elon' ],
-    'mohsenemx': [ 'Fuck linux, windows is better' ],
-    'moosedude': [ 'freedom of choice is just an illusion', 'the earth is flat', 'cheese is a right wing fantasy creation to control the left' ]
+    'roizor': {
+        opinions: [ 
+            'Twitter is better with Elon' 
+        ],
+        weight: [ 
+            0.64
+        ] // Validity, 0 is bad, 1 is best
+    },
+    'mohsenemx': {
+        opinions: [ 
+            'Fuck linux, windows is better' 
+        ],
+        weight: [ 
+            0.7 
+        ]
+    },
+    'moosedude': {
+        opinions: [ 
+            'freedom of choice is just an illusion', 
+            'the earth is flat', 
+            'cheese is a right wing fantasy creation to control the left' 
+        ],
+        weight: [
+            
+        ]
+    }
 }
+
+Object.keys(invalid).forEach(user => {
+    let userOpinions = invalid[user];
+    console.log(user, 'thinks', userOpinions)
+})
